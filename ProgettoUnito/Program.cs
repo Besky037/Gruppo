ed34f03bd3979597a7d9a740e9a4625c626e5a7b
Alessio Macrì <m.alessio02@gmail.com>
2025-05-21 17:59:28 +0200

    push Alessio main

--- ProgettoUnito/Program.cs
+++ ProgettoUnito/Program.cs
@@ -23,6 +23,27 @@ public class Corso
     }
 }
 
+public class CorsoPittura : Corso
+{
+    public string tecnica;
+
+    public CorsoPittura(string n, int du, string doc, List<string> s, string t) : base(n, du, doc, s)
+    {
+        tecnica = t;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}, Tecnica: {tecnica}";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si lavora su tela con tecnica: {tecnica}");
+    }
+}
+
+
 public class Program
 {
     public static void Main()
@@ -31,6 +52,46 @@ public class Program
         Console.WriteLine("1. Aggiungi un Corso di Musica\n2Aggiungi un corso di Pittura\n3.Aggiungi un corso di Danza\n4.Aggiungi studente a un corso\n5.Visualizza tutti i corsi\n6.Cerca Corsi per nome docente\n7.Esegui Metodo Speciale di un corso\n0.Esci");
         int scelta = int.Parse(Console.ReadLine());
 
+        bool x = true;
+        do
+        {
+
+            switch (scelta)
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+                case 7:
+
+                    break;
+
+                case 8:
+
+                    break;
+            }
+        }
+        while (x);
     }
 
     public static Corso AggiungiCorso(int scelta)
@@ -65,6 +126,7 @@ public class Program
             return new CorsoDanza(nome, ore, docente, studenti, danza);
         }
     }
-    
+
+
 }
 

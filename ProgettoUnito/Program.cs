48a48142490ba89f09d50414e3a87ad35c8965dc
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-21 19:58:24 +0200

    Aggiunta Parte di commenti

--- ProgettoUnito/Program.cs
+++ ProgettoUnito/Program.cs
@@ -2,11 +2,19 @@
 
 public class Corso
 {
+    //Definizione delle proprietà fondamentali dei corsi
     public string nomeCorso;
     public int durataOre;
     public string docente;
     protected List<string> studenti;
 
+    /// <summary>
+    /// Costruttore della classe corso per generare tutti gli oggetti della classe
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="du"></param>
+    /// <param name="doc"></param>
+    /// <param name="s"></param>
     public Corso(string n, int du, string doc, List<String> s)
     {
         nomeCorso = n;
@@ -14,11 +22,20 @@ public class Corso
         docente = doc;
         studenti = s;
     }
+
+    /// <summary>
+    /// Override del metodo della classe object toString() per la classe corsi
+    /// </summary>
+    /// <returns>Restituisce la descrizione del corso</returns>
     public override string ToString()
     {
         return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}";
     }
 
+    /// <summary>
+    /// Metodo della classe Corso per aggiungere il nome degli studenti del corso nella lista studenti
+    /// </summary>
+    /// <param name="nomeStudente"></param>
     public void AggiungiStudente(string nomeStudente)
     {
         studenti.Add(nomeStudente);
@@ -32,19 +49,37 @@ public class Corso
 
 public class CorsoMusica : Corso
 {
+    //Definizione della proprietà aggiuntiva strumento
     public string strumento;
 
+    /// <summary>
+    /// Costruttore della classe figlia CorsoMusica che prende si basa sulle proprietà della classe padre Corso
+    /// + lo strumento
+    /// </summary>
+    /// <param name="nomeCorso"></param>
+    /// <param name="durataOre"></param>
+    /// <param name="docente"></param>
+    /// <param name="studenti"></param>
+    /// <param name="str"></param>
     public CorsoMusica(string nomeCorso, int durataOre, string docente, List<string> studenti, string str)
     : base(nomeCorso, durataOre, docente, studenti)
     {
         strumento = str;
     }
 
+    /// <summary>
+    /// Override del metodo ToString() per stampare oltre al ToString Base della classe padre anche lo strumento
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
         return $"{base.ToString()} Strumento: {strumento}";
     }
 
+    /// <summary>
+    /// Metodo che stampa un testo speciale per ogni classe figlia in questo caso stampa che 
+    /// si terrà una prova pratica dello strumento del corso
+    /// </summary>
     public override void MetodoSpeciale()
     {
         Console.WriteLine($"Si tiene una prova pratica dello strumento: {strumento}");
@@ -103,7 +138,7 @@ public class Program
         do
         {
             Console.WriteLine("Benvenuto nel Menu");
-            Console.WriteLine("1. Aggiungi un Corso di Musica\n2Aggiungi un corso di Pittura\n3.Aggiungi un corso di Danza\n4.Aggiungi studente a un corso\n5.Visualizza tutti i corsi\n6.Cerca Corsi per nome docente\n7.Esegui Metodo Speciale di un corso\n0.Esci");
+            Console.WriteLine("1.Aggiungi un Corso di Musica\n2.Aggiungi un corso di Pittura\n3.Aggiungi un corso di Danza\n4.Aggiungi studente a un corso\n5.Visualizza tutti i corsi\n6.Cerca Corsi per nome docente\n7.Esegui Metodo Speciale di un corso\n0.Esci");
             int scelta = int.Parse(Console.ReadLine());
 
             switch (scelta)
@@ -150,26 +185,26 @@ public class Program
         Console.Write("Inserisci il durata ore: ");
         int ore = int.Parse(Console.ReadLine());
 
-        Console.Write("Inserisci il nome del corso: ");
+        Console.Write("Inserisci il nome del docente del corso: ");
         string docente = Console.ReadLine();
 
         if (scelta == 1)
         {
-            Console.WriteLine("Inserisci lo strumento del corso: ");
+            Console.Write("Inserisci lo strumento del corso: ");
             string strumento = Console.ReadLine();
 
             return new CorsoMusica(nome, ore, docente, studenti, strumento);
         }
         else if (scelta == 2)
         {
-            Console.WriteLine("Inserisci la tecnica del corso: ");
+            Console.Write("Inserisci la tecnica del corso: ");
             string tecnica = Console.ReadLine();
 
             return new CorsoPittura(nome, ore, docente, studenti, tecnica);
         }
         else if (scelta == 3)
         {
-            Console.WriteLine("Inserisci lo stile del corso: ");
+            Console.Write("Inserisci lo stile del corso: ");
             string danza = Console.ReadLine();
 
             return new CorsoDanza(nome, ore, docente, studenti, danza);
@@ -182,7 +217,7 @@ public class Program
         int count = 0;
         foreach (Corso c in corsiPresenti)
         {
-            Console.WriteLine($"{count}.{c}");
+            Console.WriteLine($"[{count}] {c}");
             count++;
         }
     }
@@ -190,7 +225,7 @@ public class Program
     public static void SelezionaCorso(List<Corso> corsiPresenti, int daje)
     {
         StampaCorsi(corsiPresenti);
-        Console.WriteLine("Scegli il corso: ");
+        Console.Write("Scegli il corso: ");
         int scelta = int.Parse(Console.ReadLine());
         if (scelta < 0 && scelta > corsiPresenti.Count())
         {

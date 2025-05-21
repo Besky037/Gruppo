abe4823316d7a0c11d210ecf002dfa2a0da516f8
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-05-21 18:53:10 +0200

    Update Program.cs

--- ProgettoUnito/Program.cs
+++ ProgettoUnito/Program.cs
@@ -1,5 +1,55 @@
 ﻿using System;
-using System.ComponentModel;
+
+public class Corso
+{
+    public string nomeCorso;
+    public int durataOre;
+    public string docente;
+    protected List<string> studenti;
+
+    public Corso(string n, int du, string doc, List<String> s)
+    {
+        nomeCorso = n;
+        durataOre = du;
+        docente = doc;
+        studenti = s;
+    }
+    public override string ToString()
+    {
+        return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}";
+    }
+
+    public void AggiungiStudente(string nomeStudente)
+    {
+        studenti.Add(nomeStudente);
+    }
+
+    public virtual void MetodoSpeciale()
+    {
+
+    }
+}
+
+public class CorsoMusica : Corso
+{
+    public string strumento;
+
+    public CorsoMusica(string nomeCorso, int durataOre, string docente, List<string> studenti, string str)
+    : base(nomeCorso, durataOre, docente, studenti)
+    {
+        strumento = str;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} Strumento: {strumento}";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si tiene una prova pratica dello strumento: {strumento}");
+    }
+}
 
 public class CorsoPittura : Corso
 {
@@ -12,7 +62,7 @@ public class CorsoPittura : Corso
 
     public override string ToString()
     {
-        return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}, Tecnica: {tecnica}";
+        return $"{base.ToString()} Tecnica: {tecnica}";
     }
 
     public override void MetodoSpeciale()
@@ -46,7 +96,7 @@ public class Program
                     AggiungiCorso(3)
                     break;
                 case 4:
-                    
+
                     break;
 
                 case 5:

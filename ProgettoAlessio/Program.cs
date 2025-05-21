6ed7b7138de35099aa8d6f2e1ac30ccb62685780
Alessio Macrì <m.alessio02@gmail.com>
2025-05-21 18:11:26 +0200

    org

---
+++ ProgettoAlessio/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Corso
+{
+    public string nomeCorso;
+    public int durataOre;
+    public string docente;
+    protected List<string> studenti;
+
+    public override string ToString()
+    {
+        return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}";
+    }
+
+    public void AggiungiStudente(string nomeStudente)
+    {
+        studenti.Add(nomeStudente);
+    }
+
+    public virtual void MetodoSpeciale()
+    {
+
+    }
+
+    public Corso(string n, int du, string doc, List<string> s)
+    {
+        
+    }
+}
+
+public class CorsoPittura : Corso
+{
+    public string tecnica;
+
+    public CorsoPittura(string n, int du, string doc, List<string> s, string t) : base(n, du, doc, s)
+    {
+        tecnica = t;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome Corso:{nomeCorso} Durata Ore:{durataOre} Docente: {docente}, Tecnica: {tecnica}";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si lavora su tela con tecnica: {tecnica}");
+    }
+}
